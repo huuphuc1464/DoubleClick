2d652d81ca7bdd105cd209601962d5ff366013ca
dat-rgb <trandatc3vvk@gmail.com>
2024-12-22 09:23:36 +0700

    Cập nhật thay đổi trên trang danh sách Blog

--- DoubleClick/resources/views/Blog/danhSachBlog.blade.php
+++ DoubleClick/resources/views/Blog/danhSachBlog.blade.php
@@ -1,49 +1,47 @@
-@extends('User.Blog.layoutBlog')
+@extends('Blog.layoutBlog')
 
 @section('subcontent')
-<!-- Blog entries -->
+
 <div class="w3-col l8 s12">
-  <!-- Blog entry -->
-  <div class="w3-card-4 w3-margin w3-white">
-    <img src="/w3images/woods.jpg" alt="Nature" style="width:100%">
-    <div class="w3-container">
-      <h3><b>TITLE HEADING</b></h3>
-      <h5>Title description, <span class="w3-opacity">April 7, 2014</span></h5>
-    </div>
-    <div class="w3-container">
-      <p>Mauris neque quam, fermentum ut nisl vitae...</p>
-      <div class="w3-row">
-        <div class="w3-col m8 s12">
-          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+    <!-- Bài viết -->
+    <div class="w3-card-4 w3-margin w3-white">
+        <img src="/img/Blog/Blog01.jpg" alt="Bìa sách" style="width:100%;">
+        <div class="w3-container">
+            <h3><b>Nghệ Thuật Đọc Sách</b></h3>
+            <h5>Khám phá niềm vui từ sách, <span class="w3-opacity">20 Tháng 12, 2024</span></h5>
         </div>
-        <div class="w3-col m4 w3-hide-small">
-          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+        <div class="w3-container">
+            <p>Hãy bước vào thế giới văn học đầy thú vị và khám phá cách những cuốn sách định hình góc nhìn cũng như làm phong phú cuộc sống của chúng ta...</p>
+            <div class="w3-row">
+                <div class="w3-col m8 s12">
+                    <p><button class="w3-button w3-padding-large w3-white w3-border"><b>ĐỌC TIẾP »</b></button></p>
+                </div>
+                <div class="w3-col m4 w3-hide-small">
+                    <p><span class="w3-padding-large w3-right"><b>Bình luận  </b> <span class="w3-tag">0</span></span></p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <hr>
+    <hr>
 
-  <!-- Another Blog entry -->
-  <div class="w3-card-4 w3-margin w3-white">
-    <img src="/w3images/bridge.jpg" alt="Norway" style="width:100%">
-    <div class="w3-container">
-      <h3><b>BLOG ENTRY</b></h3>
-      <h5>Title description, <span class="w3-opacity">April 2, 2014</span></h5>
-    </div>
-    <div class="w3-container">
-      <p>Mauris neque quam, fermentum ut nisl vitae...</p>
-      <div class="w3-row">
-        <div class="w3-col m8 s12">
-          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+    <!-- Bài viết khác -->
+    <div class="w3-card-4 w3-margin w3-white">
+        <img src="/img/Blog/Blog02.jpg" alt="Bìa sách" style="width:100%">
+        <div class="w3-container">
+            <h3><b>Những Cuốn Sách Đổi Thay Cuộc Sống</b></h3>
+            <h5>Cảm hứng từ trang sách, <span class="w3-opacity">18 Tháng 12, 2024</span></h5>
         </div>
-        <div class="w3-col m4 w3-hide-small">
-          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-badge">2</span></span></p>
+        <div class="w3-container">
+            <p>Cùng khám phá những cuốn sách đã thay đổi cuộc sống của hàng triệu người trên thế giới, từ tiểu thuyết đến sách kỹ năng sống...</p>
+            <div class="w3-row">
+                <div class="w3-col m8 s12">
+                    <p><button class="w3-button w3-padding-large w3-white w3-border"><b>ĐỌC TIẾP »</b></button></p>
+                </div>
+                <div class="w3-col m4 w3-hide-small">
+                    <p><span class="w3-padding-large w3-right"><b>Bình luận  </b> <span class="w3-badge">2</span></span></p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <!-- END BLOG ENTRIES -->
 </div>
-
 @endsection
