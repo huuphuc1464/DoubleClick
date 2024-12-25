ee9779da7d4aebf456d7e2b785418079d2bfeda4
dat-rgb <trandatc3vvk@gmail.com>
2024-12-25 11:37:57 +0700

    Cập nhật thay đổi trang thanh toán: các hình thức thanh toán đưa từ controller qua view và sự kiện thay đổi hình thức thanh toán

--- DoubleClick/app/Http/Controllers/ThanhToanController.php
+++ DoubleClick/app/Http/Controllers/ThanhToanController.php
@@ -6,8 +6,16 @@ use Illuminate\Http\Request;
 
 class ThanhToanController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $title = "Thanh toán | Double Click";
-        return view('thanhToan', compact('title'));
+        $hinhThucThanhToan = [
+            [''=> 1, 'Tên' => 'Thanh toán trực tuyến VNPAY', 'HinhAnh' => 'img/vnpay.webp'],
+            [''=> 2, 'Tên' => 'Thanh toán bằng thẻ quốc tế Visa, Master', 'HinhAnh' => 'img/visa.webp'],
+            [''=> 3, 'Tên' => 'Thẻ ATM nội địa/Internet Banking (Hỗ trợ Internet Banking)', 'HinhAnh' => 'img/atm.webp'],
+            [''=> 4, 'Tên' => 'Thanh toán khi nhận hàng (COD)', 'HinhAnh' => 'img/cod.webp']
+        ];
+        return view('thanhToan', compact('title', 'hinhThucThanhToan'));
     }
+
 }
