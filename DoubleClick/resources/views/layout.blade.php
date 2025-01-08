43f9caca5297164a20105047775c291944a51b02
TranHuuPhuc <0306221464@caothang.edu.vn>
2025-01-08 18:22:16 +0700

    update trang cá nhân

--- DoubleClick/resources/views/layout.blade.php
+++ DoubleClick/resources/views/layout.blade.php
@@ -194,15 +194,7 @@
 
                                 </div>
                             </div>
-                            @if ($errors->any())
-                            <div class="errors">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            @endif
+
 
 
 
