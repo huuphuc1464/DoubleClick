a770fde18448a5eb5e9e1d19d1996bf4e05f289f
MinhTan <nguyenminhtanvta1405@gmail.com>
2025-01-08 18:19:23 +0700

    fix

--- DoubleClick/resources/views/layout.blade.php
+++ DoubleClick/resources/views/layout.blade.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="{{ asset('css/color.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
     <link rel="stylesheet" href="{{ asset('css/trangchu.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/forgotpassword.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
