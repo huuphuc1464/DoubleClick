00757852732ec8706c72e0125c3844f82d457741
dat-rgb <trandatc3vvk@gmail.com>
2025-01-09 21:37:42 +0700

    Cập nhật layout admin: Xóa dấu ; và thêm nút đăng xuất

--- DoubleClick/resources/views/Admin/layout.blade.php
+++ DoubleClick/resources/views/Admin/layout.blade.php
@@ -22,7 +22,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-    @yield('css');
+    @yield('css')
 
 </head>
 
@@ -335,15 +335,18 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Bạn muốn đăng xuất?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Chọn "Đăng xuất" bên dưới nếu bạn đã sẵn sàng kết thúc phiên làm việc hiện tại.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <form action="{{route('logout')}}" method="post">
+                        @csrf
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Hủy</button>
+                        <button class="btn btn-primary" type="submit">Đăng xuất</>
+                    </form>
                 </div>
             </div>
         </div>
